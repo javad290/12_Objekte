eeba95c791468816ca3b85a916675854e2eaad85
javad290 <javad290@gmail.com>
2022-01-25 12:29:24 +0100

    fertig

--- script_01.js
+++ script_01.js
@@ -4,8 +4,30 @@
 
 /***** Objekte 1 Daten/Funktionen *******/
 
+let person =
+{
+        firstName: "Javad",
+        familyName: "Hajian",
+        salary: [120000, 160000],
+        permission: true,
+        sayHello:
+                function () {
+                        return "Hello , ich bin " + this.firstName
+                }
 
 
+}
+
+
+
+output(person)
+output(person.firstName)
+output(person.familyName)
+
+const text = "ich bin " + person.firstName + " " + person.familyName + " und verdiene " +
+        person.salary[1] + " p.a. "
+output(text)
+output(person.sayHello())
 
 /** Ausgabe */
 function output(outputData) {
